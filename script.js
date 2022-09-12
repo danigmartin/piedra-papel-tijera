cfb325a1b7868c8f1fbc9eb1b6795f917931cfca
Danigmartin <danigmartinweb@gmail.com>
2022-09-12 15:58:05 +0200

    Creates alert Pc selected element

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let player = 0
-let pc = 0
+let pc = 1
 let playerSelectedElement;
 let pcSelectedElement;
 // 1 es piedra, 2 es papel, 3 es tijera.
@@ -22,4 +22,15 @@ if (pc == 1) {
   pcSelectedElement = "Papel";
 } else if (pc == 3) {
   pcSelectedElement = "Tijera";
+}
+
+alert("El PC eligió " + pcSelectedElement)
+
+//COMBATE
+if (pc == jugador) {
+  alert("Empate")
+} else if (jugador == 1 && pc == 3) {
+  alert("Ganaste")
+} else if (jugador == 2 && pc == 1) {
+  alert("Ganaste")
 }
